c61b4a01bbc065ecc06def3bc19a9cb1335ef965
morganhah99 <mjh58@njit.edu>
2024-05-23 18:53:01 -0400

    Separated broadcast receiver from UI

---
+++ app/src/main/java/com/example/androidmaincomponents/ContactRepository.kt
@@ -0,0 +1,130 @@
+package com.example.androidmaincomponents
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.provider.ContactsContract
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.compose.runtime.mutableStateListOf
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
+
+class ContactRepository(private val context: Context) {
+
+    companion object {
+        const val REQUEST_READ_CONTACTS: Int = 1231
+    }
+
+    private val contacts = mutableStateListOf<Contact>()
+
+    data class Contact(
+        val name: String, val phoneNumber: String?, val contactID: String?, val email: String?
+    )
+
+    fun loadContacts(activity: MainActivity, rootView: View): List<Contact> {
+        if (ActivityCompat.checkSelfPermission(
+                context, Manifest.permission.READ_CONTACTS
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            val contacts = getContactList(context)
+            Log.d("Contacts", contacts.joinToString(separator = "\n"))
+            // Toast.makeText(context, "Success!", Toast.LENGTH_SHORT).show()
+        } else {
+            requestContactsPermission(activity, rootView)
+            Toast.makeText(context, "Permission needed!", Toast.LENGTH_SHORT).show()
+        }
+        return contacts
+
+    }
+
+
+    private fun requestContactsPermission(activity: MainActivity, rootView: View) {
+        if (ContextCompat.checkSelfPermission(
+                context, Manifest.permission.READ_CONTACTS
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            //Log.d("Contacts", contacts.joinToString(separator = "\n"))
+            Toast.makeText(context, "Success!", Toast.LENGTH_SHORT).show()
+        } else {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    activity, Manifest.permission.READ_CONTACTS
+                )
+            ) {
+                Snackbar.make(
+                    rootView,
+                    "The app needs permission to access your contacts.",
+                    Snackbar.LENGTH_INDEFINITE
+                ).setAction("OK") {
+                    ActivityCompat.requestPermissions(
+                        activity, arrayOf(Manifest.permission.READ_CONTACTS), REQUEST_READ_CONTACTS
+                    )
+                }.show()
+            } else {
+                ActivityCompat.requestPermissions(
+                    activity, arrayOf(Manifest.permission.READ_CONTACTS), REQUEST_READ_CONTACTS
+                )
+            }
+        }
+    }
+
+    @SuppressLint("Range")
+    fun getContactList(context: Context): List<Contact> {
+        val contentResolver = context.contentResolver
+        val cursor = contentResolver.query(
+            ContactsContract.Contacts.CONTENT_URI, null, null, null, null
+        )
+        cursor?.use {
+            while (it.moveToNext()) {
+                val id = it.getString(it.getColumnIndex(ContactsContract.Contacts._ID))
+                val name = it.getString(it.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME))
+                val hasPhoneNumber =
+                    it.getInt(it.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER)) > 0
+                if (hasPhoneNumber) {
+                    val phoneCursor = contentResolver.query(
+                        ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                        null,
+                        ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
+                        arrayOf(id),
+                        null
+                    )
+                    var email: String? = null
+                    val emailCursor = contentResolver.query(
+                        ContactsContract.CommonDataKinds.Email.CONTENT_URI,
+                        null,
+                        ContactsContract.CommonDataKinds.Email.CONTACT_ID + " = ?",
+                        arrayOf(id),
+                        null
+                    )
+                    emailCursor?.use { ec ->
+                        if (ec.moveToNext()) {
+                            email =
+                                ec.getString(ec.getColumnIndex(ContactsContract.CommonDataKinds.Email.ADDRESS))
+                        }
+                    }
+                    phoneCursor?.use { pc ->
+                        while (pc.moveToNext()) {
+                            val phoneNumber =
+                                pc.getString(pc.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))
+                            val contactID =
+                                pc.getString(pc.getColumnIndex(ContactsContract.CommonDataKinds.Phone.CONTACT_ID))
+                            val contact = Contact(name, phoneNumber, contactID, email)
+                            contacts.add(contact)
+                        }
+                    }
+                    phoneCursor?.close()
+                } else {
+                    val contact = Contact(name, null, null, null)
+                    contacts.add(contact)
+                }
+            }
+        }
+        cursor?.close()
+        return contacts
+    }
+
+
+}
--- app/src/main/java/com/example/androidmaincomponents/MainActivity.kt
+++ app/src/main/java/com/example/androidmaincomponents/MainActivity.kt
@@ -5,7 +5,6 @@ import android.content.IntentFilter
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -16,15 +15,10 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import com.example.androidmaincomponents.ui.theme.AndroidMainComponentsTheme
-import com.google.android.material.snackbar.Snackbar
-import android.Manifest
-import android.annotation.SuppressLint
 import android.app.ActivityManager
 import android.content.Context
-import android.provider.ContactsContract
+import android.view.View
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -39,6 +33,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -51,9 +46,9 @@ import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
 
-    private val REQUEST_READ_CONTACTS: Int = 1231
 
-    private val contacts = mutableListOf<Contact>()
+    private val contactRepository by lazy { ContactRepository(this) }
+    private val contacts = mutableStateListOf<ContactRepository.Contact>()
 
 
     @RequiresApi(Build.VERSION_CODES.TIRAMISU)
@@ -84,7 +79,9 @@ class MainActivity : ComponentActivity() {
         val intent = Intent("com.example.androidmaincomponents.MainBroadcastReceiver")
         sendBroadcast(intent)
 
-        loadContacts()
+        val rootView = findViewById<View>(android.R.id.content)
+        contacts.addAll(contactRepository.loadContacts(this, rootView))
+
     }
 
     @Composable
@@ -98,7 +95,7 @@ class MainActivity : ComponentActivity() {
     }
 
     @Composable
-    fun ContactCardView(contact: Contact) {
+    fun ContactCardView(contact: ContactRepository.Contact) {
         Card(
             modifier = Modifier
                 .padding(8.dp)
@@ -205,135 +202,17 @@ class MainActivity : ComponentActivity() {
         return false
     }
 
-
-    private fun loadContacts() {
-        if (ActivityCompat.checkSelfPermission(
-                this, Manifest.permission.READ_CONTACTS
-            ) == PackageManager.PERMISSION_GRANTED
-        ) {
-            val contacts = getContactList(this)
-            Log.d("Contacts", contacts.joinToString(separator = "\n"))
-//            Toast.makeText(this, "Success!", Toast.LENGTH_SHORT).show()
-        } else {
-            requestContactsPermission()
-            Toast.makeText(this, "Permission!", Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    private fun requestContactsPermission() {
-        // Check if the permission has already been granted
-        if (ContextCompat.checkSelfPermission(
-                this, Manifest.permission.READ_CONTACTS
-            ) == PackageManager.PERMISSION_GRANTED
-        ) {
-            // Permission has already been granted, do something with the contact list
-            val contacts = getContactList(this)
-            Log.d("Contacts", contacts.joinToString(separator = "\n"))
-            Toast.makeText(this, "Success!", Toast.LENGTH_SHORT).show()
-            // Do something with the contact list
-        } else {
-            // Permission has not been granted, request it
-            if (ActivityCompat.shouldShowRequestPermissionRationale(
-                    this, Manifest.permission.READ_CONTACTS
-                )
-            ) {
-                // Explain why the app needs the permission
-                // You can show a dialog or a Snackbar here
-                Snackbar.make(
-                    findViewById(android.R.id.content),
-                    "The app needs permission to access your contacts.",
-                    Snackbar.LENGTH_INDEFINITE
-                ).setAction("OK") {
-                    // Request the permission
-                    ActivityCompat.requestPermissions(
-                        this, arrayOf(Manifest.permission.READ_CONTACTS), REQUEST_READ_CONTACTS
-                    )
-                }.show()
-            } else {
-                // Request the permission
-                ActivityCompat.requestPermissions(
-                    this, arrayOf(Manifest.permission.READ_CONTACTS), REQUEST_READ_CONTACTS
-                )
-            }
-        }
-    }
-
     override fun onRequestPermissionsResult(
         requestCode: Int, permissions: Array<String>, grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == REQUEST_READ_CONTACTS && grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            // Permission has been granted, do something with the contact list
-            val contacts = getContactList(this)
-            Log.d("Contacts", contacts.joinToString(separator = "\n"))
+        if (requestCode == ContactRepository.REQUEST_READ_CONTACTS && grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            contacts.addAll(contactRepository.getContactList(this))
             Toast.makeText(this, "Success!", Toast.LENGTH_SHORT).show()
-            // Do something with the contact list
         } else {
-            // Permission has been denied
-            // You can show a dialog or a Snackbar here to explain why the app needs the permission
+            Toast.makeText(this, "Permission denied!", Toast.LENGTH_SHORT).show()
         }
     }
-
-
-    @SuppressLint("Range")
-    fun getContactList(context: Context): List<Contact> {
-        val contentResolver = context.contentResolver
-        val cursor = contentResolver.query(
-            ContactsContract.Contacts.CONTENT_URI, null, null, null, null
-        )
-        cursor?.use {
-            while (it.moveToNext()) {
-                val id = it.getString(it.getColumnIndex(ContactsContract.Contacts._ID))
-                val name = it.getString(it.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME))
-                val hasPhoneNumber =
-                    it.getInt(it.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER)) > 0
-                if (hasPhoneNumber) {
-                    val phoneCursor = contentResolver.query(
-                        ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-                        null,
-                        ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
-                        arrayOf(id),
-                        null
-                    )
-                    var email: String? = null
-                    val emailCursor = contentResolver.query(
-                        ContactsContract.CommonDataKinds.Email.CONTENT_URI,
-                        null,
-                        ContactsContract.CommonDataKinds.Email.CONTACT_ID + " = ?",
-                        arrayOf(id),
-                        null
-                    )
-                    emailCursor?.use { ec ->
-                        if (ec.moveToNext()) {
-                            email =
-                                ec.getString(ec.getColumnIndex(ContactsContract.CommonDataKinds.Email.ADDRESS))
-                        }
-                    }
-                    phoneCursor?.use { pc ->
-                        while (pc.moveToNext()) {
-                            val phoneNumber =
-                                pc.getString(pc.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))
-                            val contactID =
-                                pc.getString(pc.getColumnIndex(ContactsContract.CommonDataKinds.Phone.CONTACT_ID))
-                            val contact = Contact(name, phoneNumber, contactID, email)
-                            contacts.add(contact)
-                        }
-                    }
-                    phoneCursor?.close()
-                } else {
-                    val contact = Contact(name, null, null, null)
-                    contacts.add(contact)
-                }
-            }
-        }
-        cursor?.close()
-        return contacts
-    }
-
-    data class Contact(
-        val name: String, val phoneNumber: String?, val contactID: String?, val email: String?
-    )
-
 
 }
 
